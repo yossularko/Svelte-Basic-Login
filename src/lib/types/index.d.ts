b4d4f09622e38fd087bbfcebb425675881abe7f6
yossularko <radenlarko@gmail.com>
2023-10-13 10:02:43 +0700

    add: types

---
+++ src/lib/types/index.d.ts
@@ -0,0 +1,16 @@
+export interface UserData {
+	___class: string;
+	lastLogin: number;
+	userStatus: string;
+	created: number;
+	accountType: string;
+	ownerId: string;
+	socialAccount: string;
+	oAuthIdentities: string | null;
+	name: string;
+	blUserLocale: string;
+	'user-token': string;
+	updated: number | null;
+	email: string;
+	objectId: string;
+}
